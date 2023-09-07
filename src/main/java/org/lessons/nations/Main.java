5b34ec4cd484a6ebbcc1ecc48a918c7d2e6a8b34
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-07 12:16:58 +0200

    MILESTONE 3 completed

--- src/main/java/org/lessons/nations/Main.java
+++ src/main/java/org/lessons/nations/Main.java
@@ -1,6 +1,8 @@
 package org.lessons.nations;
 
 import java.sql.*;
+import java.util.Scanner;
+
 
 public class Main {
 
@@ -8,16 +10,27 @@ public class Main {
     private final static String user = "root";
     private final static String password = "Jack1997";
 
-    private final static String sql = "select country_id, countries.name as nome_paese, regions.name as nome_regione, continents.name as nome_continente from countries \n" +
+    private final static String sql = "select countries.country_id, countries.name as nome_paese, regions.name as nome_regione, continents.name as nome_continente from countries \n" +
             "join regions on regions.region_id = countries.region_id\n" +
-            "join continents on continents.continent_id = regions.region_id \n" +
+            "join continents on continents.continent_id = regions.continent_id \n" +
+            "where countries.name like ?\n" +
             "order by nome_paese;";
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("quale paese vuoi cercare?");
+        String search = scan.nextLine();
+        System.out.println("cerco per: " + "'" + search + "'");
+
+
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
 
+
             try(PreparedStatement ps = conn.prepareStatement(sql)){
+
+                ps.setString(1, "%" + search + "%");
 
                try(ResultSet rs = ps.executeQuery()){
                    System.out.println("COUNTRY" +" | " + "ID" + " | " + "REGIONE"+ " | " + "CONTINENT");
